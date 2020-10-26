5371ba091170903c3b0f999dacc9b03ac1b2b20e
haolinmeri <lin.hao@meri.co>
2020-10-26 15:21:18 +0800

    Create imade.ts

---
+++ imade.ts
@@ -0,0 +1,15 @@
+let count: Number = 1
+
+console.log(typeof count)
+
+interface xiaojiejie {
+  uname: String,
+  age: Number
+}
+
+const xiaohong: xiaojiejie = {
+  uname: 'xiaohong',
+  age: 18
+}
+
+console.log(typeof xiaohong)
